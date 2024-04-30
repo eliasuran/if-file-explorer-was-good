c57dc1a24b2f912f94bfbac2ca39f067bedaa69e
Elias Rissanen Uran <elura001@osloskolen.no>
2024-04-30 23:50:34 +0200

    reading users root file system

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,27 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::{ops::Deref, path::PathBuf};
+
+use tauri::api::dir::read_dir;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn read_fs() -> Vec<String> {
+    let path = PathBuf::from(r"/Users/elura001/");
+
+    let mut root: Vec<String> = vec![String::from("xdd")];
+
+    for entry in read_dir(path, false).unwrap() {
+        let item = entry.path.deref();
+        root.push(item.to_str().unwrap_or("NOT_FOUND").to_string());
+    }
+    root
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![read_fs])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
