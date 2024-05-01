0e9629570bea9114857d3973aed5b7c5cdaaf884
Elias Rissanen Uran <elura001@osloskolen.no>
2024-05-01 09:26:51 +0200

    displaying files + toggle showing dotfiles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import { invoke } from '@tauri-apps/api/tauri';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [showDotFiles, setShowDotFiles] = useState(false);
+  const [files, setFiles] = useState([] as string[][]);
+
   useEffect(() => {
-    invoke('read_fs').then((fs) => console.log(fs));
+    invoke('read_fs').then((files) => setFiles(files as string[][]));
   }, []);
-  return <div></div>;
+  return (
+    <main>
+      <button onClick={() => setShowDotFiles(!showDotFiles)}>
+        {showDotFiles ? 'hide' : 'show'} dotfiles
+      </button>
+      <div className='file-wrapper'>
+        {files.map((file) => (
+          <>
+            {showDotFiles ? (
+              <span>{file[0]}</span>
+            ) : (
+              file[0].split('/')[3][0] !== '.' && <span>{file[0]}</span>
+            )}
+          </>
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export default App;
